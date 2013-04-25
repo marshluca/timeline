411331429d64ace06a7bd9aa09d5cb91ede49262
Lin Zhang <marshluca@gmail.com>
2013-04-25 14:32:43 +0800

    set default duration for Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event
   include Mongoid::Document
   field :title, type: String
   field :category, type: String
-  field :duration, type: Integer
+  field :duration, type: Integer, :default => 0
   field :description, type: String
 end
