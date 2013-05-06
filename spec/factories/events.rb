cdf701f09cb59fa34759ce862553b48b96fbce26
Lin Zhang <marshluca@gmail.com>
2013-05-06 16:58:50 +0800

    rails g scaffold Event title category duration:integer description

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :event do
+    title "MyString"
+    category "MyString"
+    duration 110
+    description "MyString"
+  end
+end
