d2d1a8dfcf473dc6723ea45efc321715f2e7f987
Lin Zhang <marshluca@gmail.com>
2013-05-07 10:16:28 +0800

    fixed valide attributes for build an event

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -23,7 +23,14 @@ describe EventsController do
   # This should return the minimal set of attributes required to create a valid
   # Event. As you add validations to Event, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) do
+    {
+      "title"       => "MyString",
+      "category"    => "MyCategory",
+      "duration"    => 100,
+      "description" => "MyDescription"
+    }
+  end
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
