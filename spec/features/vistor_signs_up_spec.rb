790352bbdf79467db2f758600429404f92a385eb
Lin Zhang <marshluca@gmail.com>
2013-05-07 16:35:35 +0800

    move sign in and sign up into support module

--- spec/features/vistor_signs_up_spec.rb
+++ spec/features/vistor_signs_up_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Vistor signs up' do
   scenario 'with valid email and password' do
-    sign_up_with 'valid@example.com', 'password'
+    sign_up_with 'nilgnahz@gmail.com', 'zaq12wsx'
     expect(page).to have_content('Home#index')
   end
 
@@ -12,7 +12,7 @@ feature 'Vistor signs up' do
   end
 
   scenario 'with blank password' do
-    sign_up_with 'valid@example.com', ''
+    sign_up_with 'nilgnahz@gmail', ''
     expect(page).to have_content('Sign in')
   end
 
@@ -20,7 +20,34 @@ feature 'Vistor signs up' do
     visit new_user_registration_path
     fill_in 'Email', with: email
     fill_in 'Password', with: password
-    fill_in 'Password confirmatin', with: password
+    fill_in 'Password confirmation', with: password
     click_button 'Sign up'
+  end
+end
+
+feature 'Vistor signs in' do
+  before do
+    User.create(
+      :email => 'marshluca@gmail.com',
+      :password => 'zaq12wsx',
+      :password_confirmation => 'zaq12wsx'
+    )
+  end
+
+  scenario 'with valid email and password' do
+    sign_in_with 'marshluca@gmail.com', 'zaq12wsx'
+    expect(page).to have_content('Home#index')
+  end
+
+  scenario 'with incorrect password' do
+    sign_in_with 'marshluca@gmail', 'password'
+    expect(page).to have_content('Sign in')
+  end
+
+  def sign_in_with(email, password)
+    visit new_user_session_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Sign in'
   end
 end
