92d2108524449985c84c797db0d7707d92eb236c
greg <greg@ubuntu.(none)>
2013-05-29 11:40:20 +0800

    add user edit profile functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def edit
-    @user = User.find(1)
-    #@user = current_user
+    @user = current_user
 
     respond_to do |format|
       format.html # edit.html.erb
       format.json { render json: @user }
+    end
+  end
+
+  def update
+    @user = current_user
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to :back, notice: 'user was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 end
