497f4d06c5b4fe35dee99d800a9654366208444f
Greg <gouwa5700@gmail.com>
2013-09-17 14:42:11 +0800

    add empty rspec test for event api

---
+++ spec/APIs/events.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Events::API do
+  describe "get event list" do
+    it 'should get a list of latest events limited 20 records' do
+      # write test code here
+
+    end
+  end
+end
