97c959dc99a594ee57839098899db956f052fcb3
Greg <gouwa5700@gmail.com>
2013-09-17 15:25:05 +0800

    add rspec test for get event list and get an event

--- spec/APIs/events.rb
+++ spec/APIs/events.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
-describe Events::API do
+describe Events::API, :type => :feature, :driver => :selenium do
   describe "get event list" do
     it 'should get a list of latest events limited 20 records' do
-      # write test code here
+      visit 'http://localhost:3000/timeline/v1/events'
+      page.should have_content 'event list'
+    end
+  end
 
+  describe "get an event" do
+    it 'should get an event by id' do
+      @id = '1'
+      visit 'http://localhost:3000/timeline/v1/events/'+@id
+      page.should have_content 'event '+@id
     end
   end
 end
