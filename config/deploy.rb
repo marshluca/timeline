82ebe9d03759e27d902195d8b37aba2d4e648862
Greg <gouwa5700@gmail.com>
2013-09-25 19:11:45 +0800

    modify exsiting config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,62 +2,61 @@ $:.unshift './lib'
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rbenv'
 
-# under lib/mina from https://github.com/alfuken/mina-rails-unicorn-nginx-god
+require 'mina/defaults'
+require 'mina/extras'
+require 'mina/god'
 require 'mina/unicorn'
+require 'mina/nginx'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+Dir['lib/mina/servers/*.rb'].each { |f| load f }
 
-set :user, 'deploy'
-set :domain, '192.168.1.14'
-set :deploy_to, '/home/deploy/www/timeline'
-set :repository, 'git@github.com:marshluca/timeline.git'
-set :branch, 'feature/automated_deployment'
+###########################################################################
+# Common settings for all servers
+###########################################################################
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/mongoid.yml', 'log']
+set :repository,         'git@github.com:marshluca/timeline.git'
+set :keep_releases,       99
+set :default_server,     'production'
 
-# Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
+###########################################################################
+# Tasks
+###########################################################################
 
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
-task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
-  invoke :'rbenv:load'
+set :server, ENV['to'] || default_server
+invoke :"env:#{server}"
 
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+namespace :faye do
+  desc "Start faye server"
+  task :start do
+    queue %[cd #{deploy_to!}/current/faye && bundle exec thin start -C thin.yml]
+  end
+
+  desc "Stop faye server"
+  task :stop do
+    queue %[cd #{deploy_to!}/current/faye && bundle exec thin stop -C thin.yml]
+  end
+
+  desc "Restart faye server"
+  task :restart do
+    queue %[cd #{deploy_to!}/current/faye && bundle exec thin restart -C thin.yml]
+  end
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
-task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/shared/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+desc "Shows logs."
+task :log do
+  queue %[cd #{deploy_to!}/current && tail -f log/production.log]
+end
 
-  queue! %[mkdir -p "#{deploy_to}/shared/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
-
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+desc "Shows logs."
+task :seed do
+  queue %[cd #{deploy_to!}/current && RAILS_ENV=production bundle exec rake db:seed]
 end
 
 desc "Deploys the current version to the server."
-task :deploy => :environment do
+task :deploy do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -65,16 +64,8 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
 
     to :launch do
-      queue "touch #{deploy_to}/tmp/restart.txt"
       invoke :'unicorn:restart'
+      # invoke :'faye:restart'
     end
   end
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
-
--- lib/mina/servers/production.rb
+++ lib/mina/servers/production.rb
@@ -1,16 +1,19 @@
 # FreeBSD
 namespace :env do
   task :production => [:environment] do
-    set :domain,              '123.123.123.123'
-    set :deploy_to,           '/home/app_user/app'
-    set :sudoer,              'sudoer_user'
-    set :user,                'app_user'
-    set :group,               'app_user'
+    set :app,              'timeline'
+    set :branch,              'feature/automated_deployment'
+    set :domain,              '192.168.1.14'
+    set :deploy_to,           '/home/deploy/www/test'
+    set :sudoer,              'deploy'
+    set :user,                'deploy'
+    set :group,               'deploy'
     # set :rvm_path,          '/usr/local/rvm/scripts/rvm'   # we don't use that. see below.
-    set :services_path,       '/usr/local/etc/rc.d'          # where your God and Unicorn service control scripts will go
-    set :nginx_path,          '/usr/local/etc/nginx'
+    set :services_path,       '/etc/init.d'          # where your God and Unicorn service control scripts will go
+    set :nginx_path,          '/etc/nginx'
     set :deploy_server,       'production'                   # just a handy name of the server
     invoke :defaults                                         # load rest of the config
+    invoke :'rbenv:load'
     # invoke :"rvm:use[#{rvm_string}]"                       # since my prod server runs 1.9 as default system ruby, there's no need to run rvm:use
   end
 end
